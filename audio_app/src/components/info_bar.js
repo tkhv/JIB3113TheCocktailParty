3ebe7c77348787304db8cc39d7ea54daeba01872
Junhaeng <jlee3571@gatech.edu>
2023-09-06 18:23:54 -0400

    made border box for info_bar

--- audio_app/src/components/info_bar.js
+++ audio_app/src/components/info_bar.js
@@ -6,6 +6,7 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import ButtonGroup from "@mui/material/ButtonGroup";
+import "../App.css";
 
 const Info_bar = () => {
   const [playbackSpeed, setPlaybackSpeed] = useState(1);
@@ -29,47 +30,55 @@ const Info_bar = () => {
   };
 
   return (
-    <Box p={4}>
-      <Grid container spacing={3} alignItems="center">
-        <Grid item xs={4}>
-          <Typography gutterBottom>
-            Playback Speed : {playbackSpeed}x
-          </Typography>
-          <Slider
-            value={playbackSpeed}
-            step={0.1}
-            min={0.5}
-            max={2}
-            onChange={handlePlaybackSpeedChange}
-            valueLabelDisplay="auto"
-          />
-        </Grid>
+    <div className="info_bar">
+      <Box sx={{ p: 2, border: "2px solid grey" }}>
+        <Grid container spacing={12} alignItems="center">
+          <Grid item xs={4}>
+            <Typography gutterBottom>
+              Playback Speed : {playbackSpeed}x
+            </Typography>
+            <Slider
+              value={playbackSpeed}
+              step={0.1}
+              min={0.5}
+              max={2}
+              onChange={handlePlaybackSpeedChange}
+              valueLabelDisplay="auto"
+            />
+          </Grid>
 
-        <Grid item xs={4}>
-          <Typography gutterBottom>
-            Zoom Level: {zoomLevel.toFixed(1)}
-          </Typography>
-          <ButtonGroup variant="contained" color="primary">
-            <Button variant="contained" color="primary" onClick={handleZoomIn}>
-              Zoom In
-            </Button>
-            <Button
-              variant="contained"
-              color="secondary"
-              onClick={handleZoomOut}
-            >
-              Zoom Out
-            </Button>
-          </ButtonGroup>
-        </Grid>
+          <Grid item xs={4}>
+            <Typography gutterBottom>
+              Zoom Level: {zoomLevel.toFixed(1)}
+            </Typography>
+            <ButtonGroup variant="contained" color="primary">
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleZoomIn}
+              >
+                Zoom In
+              </Button>
+              <Button
+                variant="contained"
+                color="secondary"
+                onClick={handleZoomOut}
+              >
+                Zoom Out
+              </Button>
+            </ButtonGroup>
+          </Grid>
 
-        <Grid item xs={4}>
-          <Typography gutterBottom>Time Range</Typography>
-          <TextField label="start time" variant="filled" />
-          <TextField label="end time" variant="filled" />
+          <Grid item xs={4}>
+            <Typography gutterBottom>Time Range</Typography>
+            <div style={{ display: "flex" }}>
+              <TextField label="start time" variant="filled" />
+              <TextField label="end time" variant="filled" />
+            </div>
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </div>
   );
 };
 
